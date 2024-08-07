28168facc0558ba27fa93db98e9a03b38ad503ba
GreyCardinal59 <110041066+GreyCardinal59@users.noreply.github.com>
2024-08-08 01:38:15 +0500

    Add files via upload

---
+++ ToDoApp/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using ToDoApp.Models;
+using ToDoApp.Services;
+using Newtonsoft.Json;
+
+namespace ToDoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string PATH = $"{Environment.CurrentDirectory}\\todoDataList.json";
+        private BindingList<ToDoModel> _todoDataList;
+        private FileIOService _fileIOService;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _fileIOService = new FileIOService(PATH);
+
+            try
+            {
+                _todoDataList = _fileIOService.LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+
+            //_todoDataList = new BindingList<ToDoModel>()
+            //{
+            //    new ToDoModel() { Text = "test" },
+            //    new ToDoModel() { Text = "djsad" }
+            //};
+
+            dgToDoList.ItemsSource = _todoDataList;
+            _todoDataList.ListChanged += _todoDataList_ListChanged;
+
+        }
+
+        private void _todoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            //switch (e.ListChangedType)
+            //{
+            //    case ListChangedType.Reset:
+            //        break;
+            //    case ListChangedType.ItemAdded:
+            //        break;
+            //    case ListChangedType.ItemDeleted:
+            //        break;
+            //    case ListChangedType.ItemMoved:
+            //        break;
+            //    case ListChangedType.ItemChanged:
+            //        break;
+            //    case ListChangedType.PropertyDescriptorAdded:
+            //        break;
+            //}
+
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemDeleted || 
+                e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                try
+                {
+                    _fileIOService.SaveData(sender);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message); 
+                    Close();
+                }
+            }
+        }
+    }
+}
